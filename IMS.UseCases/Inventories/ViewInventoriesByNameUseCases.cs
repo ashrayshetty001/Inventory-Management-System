b5108479a6902ea92a952cfe2ff3afce9517e284
Ashray Shetty <22a21.ashray@sjec.ac.in>
2026-03-09 14:48:23 +0530

    Add project files.

---
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCases.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoriesByNameUseCases
+    {
+        public IEnumerable<Inventory> ExecuteAsync(string name="")
+        {
+
+        }
+    }
+}
