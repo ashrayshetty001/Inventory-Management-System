7df310d990c2e31ac95b9a6688dad1cfb87e28e3
Ashray Shetty <22a21.ashray@sjec.ac.in>
2026-03-09 15:11:42 +0530

    Add use case and repo interface for viewing inventories by name

---
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,22 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoriesByNameUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name="")
+        {
+            return await inventoryRepository.GetInventoriesByNameAsync(name);
+        }
+    }
+}
---
+++ IMS.UseCases/PluginInterfaces/IInventoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using IMS.CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.UseCases.PluginInterfaces
+{
+    internal interface IInventoryRepository
+    {
+        Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name);
+    }
+}
